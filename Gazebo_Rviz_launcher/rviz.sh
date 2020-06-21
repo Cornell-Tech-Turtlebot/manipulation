d45c7cd152d1cb67489af959c6dd7829cecc62dd
joAQUINCE <ec833@cornell.edu>
2020-06-21 03:53:18 -0400

    add comments

--- Gazebo_Rviz_launcher/rviz.sh
+++ Gazebo_Rviz_launcher/rviz.sh
@@ -1,2 +1,5 @@
+# allows time (in seconds) for Gazebo and moveit package to launch 
 sleep 25
+
+# launch rviz in a new terminal
 gnome-terminal -e "bash -c \"roslaunch turtlebot3_manipulation_moveit_config moveit_rviz.launch; exec bash\""
