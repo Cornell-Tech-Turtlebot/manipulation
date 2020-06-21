d31c8bee5b0a869f161b02076faab9b679915b31
joAQUINCE <ec833@cornell.edu>
2020-06-21 04:01:53 -0400

    add comments

--- Gazebo_Rviz_launcher/om.sh
+++ Gazebo_Rviz_launcher/om.sh
@@ -1,13 +1,20 @@
-reset
+# set robot type
 export TURTLEBOT3_MODEL=waffle_pi
 
-
+# set file permisions and background process
 chmod 777 ./moveit.sh
 bash moveit.sh &
+
+# line below is equivalent to previous two lines but not as clean. kept in case chmod is an issue during testing
 #gnome-terminal -e "bash -c \"bash moveit.sh; exec bash\""
 
+# set file permisions and background process
 chmod 777 ./rviz.sh
 bash rviz.sh &
+
+# line below is equivalent to previous two lines but not as clean. kept in case chmod is an issue during testing
 #gnome-terminal -e "bash -c \"bash rviz.sh; exec bash\""
+
+# launch Gazebo and start simulation automatically
 roslaunch turtlebot3_manipulation_gazebo turtlebot3_manipulation_gazebo.launch paused:=false
-#
+# end of file
