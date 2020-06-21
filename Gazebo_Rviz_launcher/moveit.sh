a41ec3292f76a3ddf141754ff2413d8f04ac893f
joAQUINCE <ec833@cornell.edu>
2020-06-21 03:52:00 -0400

    add comments

--- Gazebo_Rviz_launcher/moveit.sh
+++ Gazebo_Rviz_launcher/moveit.sh
@@ -1,2 +1,5 @@
+# allows 15 seconds for Gazebo to launch
 sleep 15
+
+# launches a new terminal and launches the moveit package
 gnome-terminal -e "bash -c \"roslaunch turtlebot3_manipulation_moveit_config move_group.launch; exec bash\""
